223a5ab7e8f7b2f94c8e45d159acc52977c3b4c1
Anthony Mugiluri <anthonyhost6@gmail.com>
2021-01-07 18:38:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxOut3 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBoxOut2 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBoxOut1 = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxLink1 = new System.Windows.Forms.TextBox();
+            this.textBoxLink2 = new System.Windows.Forms.TextBox();
+            this.textBoxLink3 = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel2.Controls.Add(this.textBoxOut3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 233);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(683, 79);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBoxOut3
+            // 
+            this.textBoxOut3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBoxOut3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxOut3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBoxOut3.ForeColor = System.Drawing.Color.Lime;
+            this.textBoxOut3.Location = new System.Drawing.Point(1, 0);
+            this.textBoxOut3.Multiline = true;
+            this.textBoxOut3.Name = "textBoxOut3";
+            this.textBoxOut3.ReadOnly = true;
+            this.textBoxOut3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut3.Size = new System.Drawing.Size(680, 76);
+            this.textBoxOut3.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Controls.Add(this.textBoxOut2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 154);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(683, 79);
+            this.panel3.TabIndex = 2;
+            // 
+            // textBoxOut2
+            // 
+            this.textBoxOut2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBoxOut2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxOut2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBoxOut2.ForeColor = System.Drawing.Color.Lime;
+            this.textBoxOut2.Location = new System.Drawing.Point(1, 0);
+            this.textBoxOut2.Multiline = true;
+            this.textBoxOut2.Name = "textBoxOut2";
+            this.textBoxOut2.ReadOnly = true;
+            this.textBoxOut2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut2.Size = new System.Drawing.Size(680, 76);
+            this.textBoxOut2.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel4.Controls.Add(this.textBoxOut1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 75);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(683, 79);
+            this.panel4.TabIndex = 3;
+            // 
+            // textBoxOut1
+            // 
+            this.textBoxOut1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBoxOut1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxOut1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBoxOut1.ForeColor = System.Drawing.Color.Lime;
+            this.textBoxOut1.Location = new System.Drawing.Point(1, 3);
+            this.textBoxOut1.Multiline = true;
+            this.textBoxOut1.Name = "textBoxOut1";
+            this.textBoxOut1.ReadOnly = true;
+            this.textBoxOut1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut1.Size = new System.Drawing.Size(680, 73);
+            this.textBoxOut1.TabIndex = 1;
+            this.textBoxOut1.TextChanged += new System.EventHandler(this.textBoxOut1_TextChanged);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonSearch.Location = new System.Drawing.Point(596, 46);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Watch";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxLink1
+            // 
+            this.textBoxLink1.Location = new System.Drawing.Point(13, 20);
+            this.textBoxLink1.Name = "textBoxLink1";
+            this.textBoxLink1.Size = new System.Drawing.Size(212, 20);
+            this.textBoxLink1.TabIndex = 0;
+            // 
+            // textBoxLink2
+            // 
+            this.textBoxLink2.Location = new System.Drawing.Point(236, 20);
+            this.textBoxLink2.Name = "textBoxLink2";
+            this.textBoxLink2.Size = new System.Drawing.Size(212, 20);
+            this.textBoxLink2.TabIndex = 1;
+            this.textBoxLink2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxLink3
+            // 
+            this.textBoxLink3.Location = new System.Drawing.Point(459, 20);
+            this.textBoxLink3.Name = "textBoxLink3";
+            this.textBoxLink3.Size = new System.Drawing.Size(212, 20);
+            this.textBoxLink3.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.buttonSearch);
+            this.panel5.Controls.Add(this.textBoxLink3);
+            this.panel5.Controls.Add(this.textBoxLink1);
+            this.panel5.Controls.Add(this.textBoxLink2);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(683, 312);
+            this.panel5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(443, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paste links/addresses above to locations e.g. PC, Documents, Desktop you want to " +
+    "monitor.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 312);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel5);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spy Locator by Mugiluri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxLink1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBoxOut1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBoxLink3;
+        private System.Windows.Forms.TextBox textBoxLink2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBoxOut3;
+        private System.Windows.Forms.TextBox textBoxOut2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public void watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            //textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = Environment.NewLine + string.Format("File[Renamed]: {0} renamed to {1} at time: {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime())));
+            textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = string.Format("File[Renamed]: {0} renamed to {1} at time: {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            //textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = Environment.NewLine + string.Format("File[Deleted]: {0} Deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+            textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = string.Format("File[Deleted]: {0} Deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = Environment.NewLine + string.Format("File[Created]: {0} Created at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));// e.FullPath,
+            textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = string.Format("File[Created]: {0} Created at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));// e.FullPath,
+        }
+
+        public void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            //textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = String.Concat(Environment.NewLine, string.Format("File[Changed]: {0} Changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime()))));// e.Name,
+            textBoxOut1.Invoke((MethodInvoker)(() => textBoxOut1.Text = string.Format("File[Changed]: {0} Changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+
+        public void watcher2_Deleted(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut2.Invoke((MethodInvoker)(() => textBoxOut2.Text = string.Format("File[Deleted]: {0} Deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher2_Renamed(object sender, RenamedEventArgs e)
+        {
+            textBoxOut2.Invoke((MethodInvoker)(() => textBoxOut2.Text = string.Format("File[Renamed]: {0} renamed to {1} at time: {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher2_Created(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut2.Invoke((MethodInvoker)(() => textBoxOut2.Text = string.Format("File[Created]: {0} Created at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher2_Changed(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut2.Invoke((MethodInvoker)(() => textBoxOut2.Text = string.Format("File[Changed]: {0} Changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+
+        public void watcher3_Renamed(object sender, RenamedEventArgs e)
+        {
+            textBoxOut3.Invoke((MethodInvoker)(() => textBoxOut3.Text = string.Format("File[Renamed]: {0} renamed to {1} at time: {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher3_Deleted(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut3.Invoke((MethodInvoker)(() => textBoxOut3.Text = string.Format("File[Deleted]: {0} Deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher3_Created(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut3.Invoke((MethodInvoker)(() => textBoxOut3.Text = string.Format("File[Created]: {0} Created at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public void watcher3_Changed(object sender, FileSystemEventArgs e)
+        {
+            textBoxOut3.Invoke((MethodInvoker)(() => textBoxOut3.Text = string.Format("File[Changed]: {0} Changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime())));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (textBoxLink1.Text != "")
+            {
+                string link1 = textBoxLink1.Text;
+                char at = '@';
+                char ex = '"';
+                string newlink1 = at + ex + link1;
+                newlink1 = newlink1.Substring(2);//Trim the first 2 characters. It was 98
+                try
+                {
+                    FileSystemWatcher watcher = new FileSystemWatcher(newlink1); //(@"C:\");//at+ex+link1+ex //C:\Users\uert\Documents
+                    watcher.EnableRaisingEvents = true;
+                    watcher.IncludeSubdirectories = true;
+
+                    //Add event handlers
+                    watcher.Changed += watcher_Changed;
+                    watcher.Created += watcher_Created;
+                    watcher.Deleted += watcher_Deleted;
+                    watcher.Renamed += watcher_Renamed;
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Use the following format C:\\ or C:\\Users\\[userName]\\Documents or C:\\Users\\[userName]\\Desktop");//Escape backslah with another backslash
+                }
+
+            }
+            if(textBoxLink2.Text != "")
+            {
+                string link2 = textBoxLink2.Text;
+                char at = '@';
+                char ex = '"';
+                string newlink2 = at + ex + link2;
+                newlink2 = newlink2.Substring(2);//Trim the first 2 characters. It was 98
+                try
+                {
+                    FileSystemWatcher watcher2 = new FileSystemWatcher(newlink2); //(@"C:\");//at+ex+link1+ex //C:\Users\uert\Documents
+                    watcher2.EnableRaisingEvents = true;
+                    watcher2.IncludeSubdirectories = true;
+
+                    //Add event handlers
+                    watcher2.Changed += watcher2_Changed;
+                    watcher2.Created += watcher2_Created;
+                    watcher2.Deleted += watcher2_Deleted;
+                    watcher2.Renamed += watcher2_Renamed;
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Use the following format C:\\ or C:\\Users\\[userName]\\Documents or C:\\Users\\[userName]\\Desktop");
+                }
+
+            }
+            if (textBoxLink3.Text != "")
+            {
+                string link3 = textBoxLink3.Text;
+                char at = '@';
+                char ex = '"';
+                string newlink3 = at + ex + link3;
+                newlink3 = newlink3.Substring(2);//Trim the first 2 characters. It was 98
+                try
+                {
+                    FileSystemWatcher watcher3 = new FileSystemWatcher(newlink3); //(@"C:\");//at+ex+link1+ex //C:\Users\uert\Documents
+                    watcher3.EnableRaisingEvents = true;
+                    watcher3.IncludeSubdirectories = true;
+
+                    //Add event handlers
+                    watcher3.Changed += watcher3_Changed;
+                    watcher3.Created += watcher3_Created;
+                    watcher3.Deleted += watcher3_Deleted;
+                    watcher3.Renamed += watcher3_Renamed;
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Use the following format C:\\ or C:\\Users\\[userName]\\Documents or C:\\Users\\[userName]\\Desktop");
+                }
+
+            }
+            else { }//Do Nothing
+        }
+
+        private void textBoxOut1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
